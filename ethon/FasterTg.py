44899b65a87f83e2dbded09dd258fec04523cf69
Vijay34j <151818969+Vijay34j@users.noreply.github.com>
2024-05-20 16:14:48 +0530

    Update FasterTg.py

--- ethon/FasterTg.py
+++ ethon/FasterTg.py
@@ -378,8 +378,8 @@ async def download_file(
     out: BinaryIO,
     progress_callback: callable = None,
 ) -> BinaryIO:
-    size = location.size
     dc_id, location = utils.get_input_location(location)
+    size = location.size
     # We lock the transfers because telegram has connection count limits
     downloader = ParallelTransferrer(client, dc_id)
     downloaded = downloader.download(location, size)
