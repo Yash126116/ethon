3d658e8b6429a8ec7ace83c61fa20afbd6929c27
Yash126116 <86655251+Yash126116@users.noreply.github.com>
2025-01-10 11:27:10 +0530

    Update mystarts.py

--- ethon/mystarts.py
+++ ethon/mystarts.py
@@ -4,10 +4,10 @@ from telethon import events, Button
 async def start_srb(event, st):
     await event.reply(st, 
                       buttons=[
-                              [Button.url("🤖 ᴜᴘᴅᴀᴛᴇ", url="https://t.me/VJ_Botz"),
-                               Button.url("🔍 sᴜᴘᴘᴏʀᴛ", url="https://t.me/vj_bot_disscussion")],
-                              [Button.url("❣️ ᴅᴇᴠᴇʟᴏᴘᴇʀ", url="t.me/KingVJ01")],
-                              [Button.url("💝 sᴜʙsᴄʀɪʙᴇ ʏᴏᴜᴛᴜʙᴇ ᴄʜᴀɴɴᴇʟ", url="https://youtube.com/@Tech_VJ")]]) 
+                              [Button.url("🤖 ᴜᴘᴅᴀᴛᴇ", url="https://t.me/Cs_Bots"),
+                               Button.url("🔍 sᴜᴘᴘᴏʀᴛ", url="https://t.me/+PmoAKnNbjk8yZTk1")],
+                              [Button.url("❣️ ᴅᴇᴠᴇʟᴏᴘᴇʀ", url="t.me/CashScopebot")],
+                              [Button.url("💝 sᴜʙsᴄʀɪʙᴇ ʏᴏᴜᴛᴜʙᴇ ᴄʜᴀɴɴᴇʟ", url="https://youtube.com")]]) 
     
 async def vc_menu(event):
     await event.edit("**VIDEO CONVERTOR v1.4**", 
@@ -16,5 +16,5 @@ async def vc_menu(event):
                          Button.inline("SOURCE", data="source")],
                         [Button.inline("NOTICE.", data="notice"),
                          Button.inline("Main.", data="help")],
-                        [Button.url("DEVELOPER", url="t.me/Kingvj01")]])
+                        [Button.url("DEVELOPER", url="t.me/CashScopebot")]])
     
